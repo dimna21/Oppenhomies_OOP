d2f397fa7e2c8a828596a97b71501e2dd6810ed3
alexandre razmadze <arazm21@freeuni.edu.ge>
2024-06-28 22:01:06 +0400

    updated

--- QuizWebapp/src/main/java/DBpackage/Questions/Question.java
+++ QuizWebapp/src/main/java/DBpackage/Questions/Question.java
@@ -3,16 +3,42 @@ package DBpackage.Questions;
 public class Question {
     private int type;
     private int quizID;
-    private int questionNumber;
+    private int subID;
+    private int questionID;
 
-    public Question(int type, int quizID, int questionNumber){
+    public Question(int questionID, int quizID, int subID, int type){
+        this.questionID=questionID;
         this.type = type;
         this.quizID = quizID;
-        this.questionNumber = questionNumber;
+        this.subID = subID;
+    }
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    public int getSubID() {
+        return subID;
+    }
+
+    public void setQuizID(int quizID) {
+        this.quizID = quizID;
+    }
+
+    public void setSubID(int subID) {
+        this.subID = subID;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 
     public int getQuestionNumber() {
-        return questionNumber;
+        return subID;
     }
 
     public int getQuizID() {

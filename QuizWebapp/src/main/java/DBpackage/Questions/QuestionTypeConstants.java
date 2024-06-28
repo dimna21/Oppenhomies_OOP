517de4ba43a9bb726fd74a0b8eb27ba6758d8fdd
dimna21 <dimna21@freeuni.edu.ge>
2024-06-28 21:54:12 +0400

    Updated Database

--- QuizWebapp/src/main/java/DBpackage/Questions/QuestionTypeConstants.java
+++ QuizWebapp/src/main/java/DBpackage/Questions/QuestionTypeConstants.java
@@ -5,4 +5,8 @@ public class QuestionTypeConstants {
     public int QUESTION_MULTIPLE_CHOICE = 2;
     public int QUESTION_PICTURE = 3;
     public int QUESTION_TEXTBOX = 4;
+    public int QUESTION_MULTIFILL_BLANK = 5;
+    public int QUESTION_MULTI_MULTIPLE_CHOICE = 6;
+    public int QUESTION_MATCHING = 7;
+
 }

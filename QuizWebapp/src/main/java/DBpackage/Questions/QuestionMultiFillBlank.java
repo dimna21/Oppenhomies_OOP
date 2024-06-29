ec7f17c4d646061dfab287487715698d238b9219
dimna21 <dimna21@freeuni.edu.ge>
2024-06-29 14:40:53 +0400

    Updated question classes

--- QuizWebapp/src/main/java/DBpackage/Questions/QuestionMultiFillBlank.java
+++ QuizWebapp/src/main/java/DBpackage/Questions/QuestionMultiFillBlank.java
@@ -5,17 +5,24 @@ import DBpackage.DatabaseInfo;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class QuestionMultipleChoice extends Question{
-    private String question;
-    private String correctAnswer;
+public class QuestionMultiFillBlank extends Question{
+    private String textBefore;
+    private String textAfter;
     private ArrayList<String> answerList;
-    public QuestionMultipleChoice( int questionID, int quizID,int subID, int type, String question, String correctAnswer) throws SQLException, ClassNotFoundException {
+    public QuestionMultiFillBlank(int questionID, int quizID,int subID, int type, String textBefore, String textAfter) {
         super(questionID, quizID, subID, type);
-        this.question = question;
-        this.correctAnswer = correctAnswer;
-        initAnswerList();
+        this.textBefore = textBefore;
+        this.textAfter = textAfter;
     }
-    private void initAnswerList() throws ClassNotFoundException, SQLException {
+    public String getTextBefore() {
+        return textBefore;
+    }
+
+    public String getTextAfter() {
+        return textAfter;
+    }
+
+    public ArrayList<String> getAnswerList() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://" +
                         DatabaseInfo.server + "/" + DatabaseInfo.database,
@@ -24,8 +31,7 @@ public class QuestionMultipleChoice extends Question{
         Statement stmt = con.createStatement();
         int quizID = this.getQuizID();
         int questionOrder = this.getQuestionNumber();
-        String query = "select answer from Multiple_choice_answers where quiz_id = '" + quizID +
-                "' and sub_id = '" + questionOrder + "';";
+        String query = "SELECT answer FROM Multi_fill_Blank_answers WHERE multifill_id = Multi_fill_Blank_answers.question_id;";
 
         try {
             ResultSet resultSet = stmt.executeQuery(query);
@@ -38,15 +44,7 @@ public class QuestionMultipleChoice extends Question{
         } finally {
             if (stmt != null) stmt.close();
         }
-    }
-
-    public ArrayList<String> getAnswerList(){
         return answerList;
     }
-    public String getQuestion(){
-        return question;
-    }
-    public String getCorrectAnswer(){
-        return correctAnswer;
-    }
+
 }
